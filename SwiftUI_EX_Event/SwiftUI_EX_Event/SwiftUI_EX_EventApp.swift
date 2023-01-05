ce80ff53a0bdc4aa40048ca29b922bffa1c69e91
eujin811 <eujin8@gmail.com>
2023-01-05 21:19:53 +0900

    :tada: swiftUI Event example

---
+++ SwiftUI_EX_Event/SwiftUI_EX_Event/SwiftUI_EX_EventApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUI_EX_EventApp.swift
+//  SwiftUI_EX_Event
+//
+//  Created by YoujinMac on 2023/01/05.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUI_EX_EventApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
